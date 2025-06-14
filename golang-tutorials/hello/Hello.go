3f6bcaf2e775896a8b73c9372d2a94d70e65b705
Vinicius Silva <vinicius92as@gmail>
2025-06-14 00:27:47 -0300

    hellows

--- golang-tutorials/hello/Hello.go
+++ golang-tutorials/hello/Hello.go
@@ -11,10 +11,23 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	// single hello
+	message, err := greetings.Hello("Vinicius")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(message)
+
+	// multiple hello's
+	names := []string{"Vinicius", "Johnny", "Sushi", "Freddy"}
+
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
+
 }
